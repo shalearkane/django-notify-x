34a3e622caee10b7515a538df4658180e6de03b0
Eden <eden.juscelino@gmail.com>
2020-08-13 19:47:15 +0100

    replace url with path

--- notify/urls.py
+++ notify/urls.py
@@ -1,19 +1,24 @@
 try:
-    from django.conf.urls import url
-except ImportError:
-    from django.urls import url
-
-from notify import views as nf
-
-app_name = 'notifications'
+    from django.conf.urls import url	    from django.conf.urls import path
+except ImportError:	except ImportError:
+    from django.urls import url	    from django.urls import path
 
 
-urlpatterns = [
-    url(r'^all/$', nf.notifications, name="all"),
-    url(r'^api/update/$', nf.notification_update, name="update"),
-    url(r'^mark/$', nf.mark, name='mark'),
-    url(r'^mark-all/$', nf.mark_all, name='mark_all'),
-    url(r'^delete/$', nf.delete, name='delete'),
-    url(r'^rdr/(?P<notification_id>[\d]+)/$', nf.read_and_redirect,
-        name='read_and_redirect'),
-]
+from notify import views as nf	from notify import views as nf
+
+
+app_name = 'notifications'	app_name = 'notifications'
+
+
+
+
+urlpatterns = [	urlpatterns = [
+    url(r'^all/$', nf.notifications, name="all"),	    path('all/', nf.notifications, name="all"),
+    url(r'^api/update/$', nf.notification_update, name="update"),	    path('api/update/', nf.notification_update, name="update"),
+    url(r'^mark/$', nf.mark, name='mark'),	    path('mark/', nf.mark, name='mark'),
+    url(r'^mark-all/$', nf.mark_all, name='mark_all'),	    path('mark-all/', nf.mark_all, name='mark_all'),
+    url(r'^delete/$', nf.delete, name='delete'),	    path('delete/<int:notification_id>/', nf.delete, name='delete'),
+    url(r'^rdr/(?P<notification_id>[\d]+)/$', nf.read_and_redirect,	    path('delete/', nf.delete, name='delete'),
+    path('rdr/<int:notification_id>/', nf.read_and_redirect,
+        name='read_and_redirect'),	        name='read_and_redirect'),
+]	]
